ae7d22e4a056f1092a0bc35a8449acd9a9937357
artem <www1@MacBook-Pro-artem.local>
2025-05-18 15:50:33 +0300

    betta V4 dit search

--- app/utils/searchFakeData.ts
+++ app/utils/searchFakeData.ts
@@ -1,5 +1,11 @@
 import { fakeData } from "./constants";
+
 export function searchFakeGroups(query: string) {
   const q = query.trim().toLowerCase();
-  return fakeData.filter((group) => group.title.toLowerCase().includes(q));
+
+  return fakeData.filter((group) => {
+    const titleMatch = group.title.toLowerCase().includes(q);
+    const aboutMatch = group.about?.toLowerCase().includes(q);
+    return titleMatch || aboutMatch;
+  });
 }
