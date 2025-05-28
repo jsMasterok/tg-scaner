803643c09341763ef84aa4c9ed4afeba421a2a55
artem <www1@MacBook-Pro-artem.local>
2025-05-28 16:37:16 +0300

    disable rules eslint

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
         <Footer />
         <Analytics />
         <Toaster />
+        <SpeedInsights />
       </body>
     </html>
   );
